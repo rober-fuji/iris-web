edba5ee302a13535557828be99db8dc4b280c0c7
Elise17 <63542039+Elise17@users.noreply.github.com>
2024-07-04 14:28:12 +0200

    [IMP] Remove unused parameter in test

--- tests/tests_rest.py
+++ tests/tests_rest.py
@@ -119,34 +119,27 @@ class TestsRest(TestCase):
         self.assertEqual(400, response.status_code)
 
     def test_get_ioc_should_return_201(self):
-        case = self._subject.create_case_deprecated()
-        case_identifier = case['case_id']
         response = self._subject.create_ioc_deprecated()
         current_identifier = response['ioc_id']
         test = self._subject.get_iocs(current_identifier)
         self.assertEqual(current_identifier, test['ioc_id'])
 
     def test_get_ioc_with_missing_ioc_identifier_should_return_400(self):
-        case = self._subject.create_case_deprecated()
-        case_identifier = case['case_id']
         self._subject.create_ioc_deprecated()
         test = self._subject.get_iocs(None)
         self.assertEqual('error', test['status'])
 
     def test_delete_ioc_should_return_201(self):
-        case = self._subject.create_case_deprecated()
-        case_identifier = case['case_id']
         response = self._subject.create_ioc_deprecated()
         current_identifier = response['ioc_id']
         self._subject.delete_iocs(current_identifier)
-        test = self._subject.get_iocs(current_identifier, case_identifier)
+        test = self._subject.get_iocs(current_identifier)
         self.assertEqual('Invalid IOC ID for this case', test)
 
     def test_delete_ioc_with_missing_ioc_identifier_should_return_400(self):
         case = self._subject.create_case_deprecated()
-        case_identifier = case['case_id']
         response = self._subject.create_ioc_deprecated()
         current_identifier = response['ioc_id']
         self._subject.delete_iocs(None)
-        test = self._subject.get_iocs(current_identifier, case_identifier)
+        test = self._subject.get_iocs(current_identifier)
         self.assertEqual(current_identifier, test['ioc_id'])
