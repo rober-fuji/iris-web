a87b9c62822eaae53468699c5c5ab1d86337ced9
Elise17 <63542039+Elise17@users.noreply.github.com>
2024-07-04 14:19:44 +0200

    [IMP] Change URL name for GET /api/v2/iocs/ioc_id (without state)

--- tests/iris.py
+++ tests/iris.py
@@ -145,8 +145,8 @@ class Iris:
     def get_iocs_deprecated(self, current_identifier):
         return self._api.get(f'/case/ioc/{current_identifier}')
 
-    def get_iocs(self, current_identifier, case_identifier):
-        return self._api.get(f'/api/v2/cases/{case_identifier}/iocs/{current_identifier}')
+    def get_iocs(self, current_identifier):
+        return self._api.get(f'/api/v2/iocs/{current_identifier}')
 
     def delete_iocs_deprecated(self, current_identifier):
         return self._api.delete(f'/case/ioc/delete/{current_identifier}')
